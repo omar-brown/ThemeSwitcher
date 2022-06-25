c89ceba5f5f048e475650d26cff28314d510c498
Omar Brown <33111209+optimaldev@users.noreply.github.com>
2022-06-25 15:06:27 -0400

    Added node server and gitignore for node_modules

---
+++ server.js
@@ -0,0 +1,31 @@
+// Require
+const express = require('express');
+const path = require('path');
+
+// Set up server constants
+const app = express();
+const router = express.Router();
+const port =  3000;
+
+// Set up app paths
+const rootPath = path.join(__dirname, 'public');
+const publicPaths = {
+    index: path.join(rootPath, 'index.html'),
+    app: path.join(rootPath, 'app.js'),
+    styles: path.join(rootPath, 'styles.css'),
+    image: path.join(rootPath, 'thunder.png')
+}
+
+// Configure HTTP GET requests
+router.get('/', (req, res) => res.sendFile(publicPaths.index));
+router.get('/app.js', (req, res) => res.sendFile(publicPaths.app));
+router.get('/styles.css', (req, res) => res.sendFile(publicPaths.styles));
+router.get('/thunder.png', (req, res) => res.sendFile(publicPaths.image));
+
+// Mount the router on the app
+app.use('/', router);
+
+// Create the server
+const server = app.listen(port, () => {
+    console.log(`App server is running on port ${port}`)
+});
